ac46698f9f5aa18479f20c9e14de4487dc739356
HostenX <94405961+HostenX@users.noreply.github.com>
2024-08-15 13:28:25 -0500

    Añadidas animaciones de movimiento de jugador
    queda pendiente ajustar animaciones a la izquierda

--- PoryectoFinalUdeC/Assets/Scripts/Movement.cs
+++ PoryectoFinalUdeC/Assets/Scripts/Movement.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Movement : MonoBehaviour
 {
 	public float speed;
+
+    public Animator animator;
     //Get input
 
     private void Update()
@@ -12,9 +15,28 @@ public class Movement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-       Vector3 direction = new Vector3(horizontal, vertical);
+       Vector3 direction = new Vector3(horizontal, vertical).normalized;
+
+        AnimateMovement(direction);
 
         transform.position += direction * speed *Time.deltaTime;
     }
     //apply movement
+
+    void AnimateMovement(Vector3 direction) {
+        if (animator != null) {
+
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+				animator.SetFloat("vertical", direction.y);
+			}
+            else
+            {
+				animator.SetBool("isMoving", false);
+			}
+        }
+    }
 }
